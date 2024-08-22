945b1c4e5a321b072b0d30a70e6b78539e59011d
DooDooFace <oneway135790@gmail.com>
2024-08-22 06:21:11 +0200

    Polishing idk

---
+++ resources/[codename-scripts]/bremselys/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "real-brake-lights"
+description "A simple resource that enables brake lights when the vehicle is stopped"
+author "Floh"
+version "1.0.0"
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
